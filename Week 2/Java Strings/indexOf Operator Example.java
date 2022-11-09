87dd2a65dc6e9cc827ae771728e9bcf6efa2f4bd
IcebergSnow <116831379+IcebergSnow@users.noreply.github.com>
2022-11-09 12:15:42 -0800

    Delete indexOf Operator Example.java

--- Week 2/Java Strings/indexOf Operator Example.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-	public static void main(String[] args) {
-		String txt = "Please locate where 'locate' occurs!";
-		System.out.println(txt.indexOf("locate")); // Outputs 7
-	}
-}
