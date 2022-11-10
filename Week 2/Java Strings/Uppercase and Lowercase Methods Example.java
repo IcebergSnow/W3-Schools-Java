256f78f3b7fe799f03a1e40a8464495c6bc9255f
IcebergSnow <116831379+IcebergSnow@users.noreply.github.com>
2022-11-10 08:14:51 -0800

    Delete Uppercase and Lowercase Methods Example.java

--- Week 2/Java Strings/Uppercase and Lowercase Methods Example.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-		String txt = "Hello World";
-		System.out.println(txt.toUpperCase());   // Outputs "HELLO WORLD"
-		System.out.println(txt.toLowerCase());   // Outputs "hello world"	
-  }
-}
